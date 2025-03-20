ccbf3c0c72b60c66559fdda7ce8e21e3a200fd3b
kazisalon <kazisawlon@gmail.com>
2025-03-20 20:15:00 +0545

    added navbar component

---
+++ student-management-system/src/app/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+// src/app/components/navbar/navbar.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  title = 'Student Management System';
+}
