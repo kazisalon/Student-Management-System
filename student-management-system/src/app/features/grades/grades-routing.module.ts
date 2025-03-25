5b882b8f8af1fe62f4bdb775b94488031e78701e
kazisalon <kazisawlon@gmail.com>
2025-03-25 20:17:39 +0545

    Added navbar component

--- student-management-system/src/app/features/grades/grades-routing.module.ts
+++ student-management-system/src/app/features/grades/grades-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'students',
+    loadChildren: () => import('./features/students/students.module').then(m => m.StudentsModule)
+  },
+  {
+    path: 'teachers',
+    loadChildren: () => import('./features/teachers/teachers.module').then(m => m.TeachersModule)
+  },
+  {
+    path: 'courses',
+    loadChildren: () => import('./features/courses/courses.module').then(m => m.CoursesModule)
+  },
+  {
+    path: 'grades',
+    loadChildren: () => import('./features/grades/grades.module').then(m => m.GradesModule)
+  },
+  { path: '', redirectTo: '/students', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class GradesRoutingModule { }
+export class AppRoutingModule { }
